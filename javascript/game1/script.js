fb308108c6cfd7663bf7996f92f3be099955ef2c
Alvyn Abranches <alvynabranches@gmail.com>
2020-10-14 19:18:18 +0530

    14102020v3

--- javascript/game1/script.js
+++ javascript/game1/script.js
@@ -2,16 +2,27 @@ const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.prepend(canvas);
 const game = {grid: 40};
-const player = {x:game.grid*7, y:game.grid*4, width:game.grid*2, height:game.grid/2};
+const player = {x:game.grid*7, y:game.grid*8, w:game.grid*2, h:game.grid/2, color:'red'};
+const keyz = {ArrowLeft:false, ArrowRight:false};
 
-canvas.setAttribute('width', grid*15);
-canvas.setAttribute('height', grid*10);
+canvas.setAttribute('width', game.grid*15);
+canvas.setAttribute('height', game.grid*10);
 canvas.style.border = '1px solid black';
+document.addEventListener('keydown', (e) => {
+    if (e.code in keyz) { keyz[e.code] = true; }
+})
+document.addEventListener('keyup', (e) => {
+    if (e.code in keyz) { keyz[e.code] = false; }
+    console.log(e.code);
+})
+
 
 draw();
 
 function draw() {
     ctx.beginPath();
-    ctx.rect(player.x, plaeyr.y, player.w, player.h);
-
+    ctx.rect(player.x, player.y, player.w, player.h);
+    ctx.fillStyle = player.color;
+    ctx.fill();
+    ctx.closePath();
 }
